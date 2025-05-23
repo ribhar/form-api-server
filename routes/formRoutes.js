b62a1655a08a6f2e3044d9fec0db4695f779048d
ribhar <ribharscode@gmail.com>
2025-05-23 19:58:19 +0530

    feat: Add PATCH and DELETE API routes for editing and deleting form entries

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { submitForm, getAllData  } = require('../controllers/formController');
+const {
+  submitForm,
+  getAllData,
+  updateEntry,
+  deleteEntry,
+} = require('../controllers/formController');
 
 const router = express.Router();
 
 router.post('/submit', submitForm);
-
 router.get('/data', getAllData);
+router.patch('/update/:id', updateEntry);
+router.delete('/delete/:id', deleteEntry);
 
 module.exports = router;
